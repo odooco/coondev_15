e8dc7519a23671cccbdaf245c0c31d4aa6d62523
coondev-michael-colorado <desarrollo@coondev.com>
2022-10-06 16:36:20 -0500

    Requerimientos

--- custom_account_segment/models/sale_order.py
+++ custom_account_segment/models/sale_order.py
@@ -10,39 +10,7 @@ _logger = logging.getLogger(__name__)
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    @api.model
-    def _search_default_journal(self, journal_types):
-        company_id = self._context.get('default_company_id', self.env.company.id)
-        domain = [('company_id', '=', company_id), ('type', 'in', journal_types)]
-
-        journal = None
-        if self._context.get('default_currency_id'):
-            currency_domain = domain + [('currency_id', '=', self._context['default_currency_id'])]
-            journal = self.env['account.journal'].search(currency_domain, limit=1)
-
-        if not journal:
-            journal = self.env['account.journal'].search(domain, limit=1)
-
-        if not journal:
-            company = self.env['res.company'].browse(company_id)
-
-            error_msg = _(
-                "No journal could be found in company %(company_name)s for any of those types: %(journal_types)s",
-                company_name=company.display_name,
-                journal_types=', '.join(journal_types),
-            )
-            raise UserError(error_msg)
-
-        return journal
-
-    def _get_default_journal(self):
-        journal_types = ['sale']
-        journal = self._search_default_journal(journal_types)
-        return journal
-
-    journal_id = fields.Many2one('account.journal', string='Journal', required=True, readonly=True,
-                                 states={'draft': [('readonly', False)]}, check_company=True,
-                                 domain="[('type', '=', 'sale')]", default=_get_default_journal)
+    journal_id = fields.Many2one('account.journal', string='Journal', domain="[('type', '=', 'sale')]")
 
     def action_view_invoice(self):
         invoices = self.mapped('invoice_ids')
