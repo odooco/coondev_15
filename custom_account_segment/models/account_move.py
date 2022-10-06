a217f3f53d25f21db325e5468ea9d11f2ab0a990
coondev-michael-colorado <desarrollo@coondev.com>
2022-10-06 17:18:16 -0500

    rc

--- custom_account_segment/models/account_move.py
+++ custom_account_segment/models/account_move.py
@@ -114,8 +114,6 @@ class AccountMove(models.Model):
                 'ref': record.ref,
                 'narration': record.ref,
                 'partner_id': record.partner_id.id,
-                'payment_mean_id': record.payment_mean_id.id,
-                'payment_mean_code_id': record.payment_mean_code_id.id,
                 'partner_shipping_id': record.partner_shipping_id.id,
                 'invoice_user_id': record.invoice_user_id.id,
                 'state': 'draft',
