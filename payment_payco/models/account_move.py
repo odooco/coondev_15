c8c291101d8f56fe325c441e0d299208b699b377
coondev-michael-colorado <desarrollo@coondev.com>
2022-10-27 22:59:13 -0500

    Correccion Epayco

--- payment_payco/models/account_move.py
+++ payment_payco/models/account_move.py
@@ -5,6 +5,7 @@ class AccountMove(models.Model):
     _inherit = "account.move"
 
     is_hide_payco_button = fields.Boolean(string="is hide payco Button", compute="_compute_is_check_enabled_for_backend_or_not")
+    check_payco_button = fields.Boolean(string="Payment Buttom")
 
     def email_payment(self):
         template = self.env.ref('payment_payco.email_template_for_epayco')
@@ -12,6 +13,7 @@ class AccountMove(models.Model):
             template.email_from = str(self.env.user.partner_id.email)
             template.attachment_ids = [(5, 0, [])]
             template.send_mail(self.id, force_send=True)
+            self.check_payco_button = True
 
     def get_invoice_details(self):
         return {
