c2bcb58c59e16ea2ec0edc0d42d49c7f2d84f75c
coondev-michael-colorado <desarrollo@coondev.com>
2022-11-11 12:27:01 -0500

    Correccion Pagos

--- custom_account_segment/models/account_payment_register.py
+++ custom_account_segment/models/account_payment_register.py
@@ -10,6 +10,39 @@ class AccountPaymentPosRegister(models.TransientModel):
     _name = 'account.payment.pos.register'
     _description = 'Register Payment Pos'
 
+    @api.model
+    def default_get(self, fields_list):
+        res = super().default_get(fields_list)
+        if 'line_ids' in fields_list and 'line_ids' not in res:
+            if self._context.get('active_model') == 'account.move.pos':
+                lines = self.env['account.move'].browse(self._context.get('active_ids', [])).line_ids
+            elif self._context.get('active_model') == 'account.move.pos.line':
+                lines = self.env['account.move.line'].browse(self._context.get('active_ids', []))
+            else:
+                raise UserError(_(
+                    "The register payment wizard should only be called on account.move or account.move.line records."
+                ))
+
+            if 'journal_id' in res and not self.env['account.journal'].browse(res['journal_id'])\
+                    .filtered_domain([('company_id', '=', lines.company_id.id), ('type', 'in', ('bank', 'cash'))]):
+                del res['journal_id']
+
+            available_lines = self.env['account.move.line']
+            for line in lines:
+                if line.move_id.state != 'posted':
+                    raise UserError(_("You can only register payment for posted journal entries."))
+                if line.account_internal_type not in ('receivable', 'payable'):
+                    continue
+                if line.currency_id:
+                    if line.currency_id.is_zero(line.amount_residual_currency):
+                        continue
+                else:
+                    if line.company_currency_id.is_zero(line.amount_residual):
+                        continue
+                available_lines |= line
+            res['line_ids'] = [(6, 0, available_lines.ids)]
+        return res
+
     @api.depends('payment_type', 'journal_id')
     def _compute_payment_method_line_fields(self):
         for wizard in self:
@@ -19,7 +52,7 @@ class AccountPaymentPosRegister(models.TransientModel):
 
     def _get_batches(self):
         self.ensure_one()
-        lines = self.line_ids._origin
+        lines = self.line_ids
 
         batches = defaultdict(lambda: {'lines': self.env['account.move.line']})
         for line in lines:
